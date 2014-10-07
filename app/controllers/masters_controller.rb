2752acdce3e8ae05ba86fd395297d415f9bed904
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-10-07 14:13:19 -0500

    added controller stuff and form for master

--- app/controllers/masters_controller.rb
+++ app/controllers/masters_controller.rb
@@ -1,4 +1,14 @@
 class MastersController < ApplicationController
   def new
+    @master = Master.new
+  end
+  
+  def create
+    @master = Master.new(params[:master])
+    if @master.save
+      redirect_to root_url, :notice => "Signed up!"
+    else
+      render :new
+    end
   end
 end
