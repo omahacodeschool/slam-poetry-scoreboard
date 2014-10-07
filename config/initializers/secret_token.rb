ac820d2af32492f65ae0a9049b4e11ab397caa94
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-10-07 13:13:50 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PoetrySlam::Application.config.secret_token = '97e0d7d69ea5ec881bd520745486f7585e8d0a2a0f1b28661597b2e6f9eb38a2f6e22556fe8baafe13f7233ccc8e34baecf179e95ac4939fc8bda4bcdbf61931'
