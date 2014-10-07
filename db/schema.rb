5ef35b97356883949ba0996d7902482a0385fca6
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-10-07 13:20:51 -0500

    slam scaffold migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20141007182019) do
+
+  create_table "slams", :force => true do |t|
+    t.integer  "master_id"
+    t.string   "name"
+    t.date     "event_date"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
 end
