3863ab37975e6ae0c91007b3dbd4e6c14d93b600
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-10-10 14:05:56 -0500

    minor spacing bug fix and got rid of poet seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)'
 Master.create(email:"grandmasterflash@slam.com", password:"password", password_confirmation:"password")
-
-poets = Poet.create([{ name: 'Franny', photo: "/uploads/poet/photo/2/Franny.jpg"}, {name: 'Shane', photo: "/uploads/poet/photo/4/shane-koyczan-2012"}, {name: 'Anonymous', photo: "/uploads/poet/photo/3/thumb_Poetry-Slam" }, {name: 'Maggie', photo: "/uploads/poet/photo/5/Maggie_ESTEP-obit-master675"}])
