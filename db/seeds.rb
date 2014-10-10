7f85dbdabb4cb4fcf5a45450036d8be54f4c60ff
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-10-10 10:13:47 -0500

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)'
 Master.create(email:"grandmasterflash@slam.com", password:"password", password_confirmation:"password")
 
-poets = Poet.create([{ name: 'Kyle'}, {name: 'Katie'}, {name: 'Andrew'}, {name: 'Sumeet'}])
+poets = Poet.create([{ name: 'Franny', photo: "/uploads/poet/photo/2/Franny.jpg"}, {name: 'Shane', photo: "/uploads/poet/photo/4/shane-koyczan-2012"}, {name: 'Anonymous', photo: "/uploads/poet/photo/3/thumb_Poetry-Slam" }, {name: 'Maggie', photo: "/uploads/poet/photo/5/Maggie_ESTEP-obit-master675"}])
