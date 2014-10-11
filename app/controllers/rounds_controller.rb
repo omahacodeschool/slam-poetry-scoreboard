e8a47ca406ad83237b1783744db154397c9483e0
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-10-11 13:46:08 -0500

    fixed flow of slam, but final_slam still has errors

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,4 +1,5 @@
 class RoundsController < ApplicationController
+  
   def index
     @rounds = Round.all
 
@@ -67,7 +68,7 @@ class RoundsController < ApplicationController
     @round = Round.find(params[:id])
   end
   
-  # Updates performances for round with scores from score acion
+  # Updates performances for round with scores from score action
   def save_score
     @round = Round.find(params[:id])
     @round.update_attributes(params[:round])
@@ -85,7 +86,6 @@ class RoundsController < ApplicationController
     
     if params[:advance_round]
       @poets = []
-   
       @slam = @prevrnd.slam
       @newrnd = @slam.rounds.create(round_number: @prevrnd.round_number.to_i + 1)
       @newrnd.assign_poets(params[:round][:poet])
